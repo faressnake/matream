90fee5ffe4e5d87b8b0750b60dac908c31de3f40
faressnake <132618855+faressnake@users.noreply.github.com>
2025-12-19 15:35:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
-import './styles/globals.css'
-import './styles/pages.css'
-import './styles/components.css'
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/globals.css";
+import "./styles/pages.css";
+import "./styles/components.css";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
